fbfa0139fbea65c32fc97dc6eaffa10f73e12349
saivojjala <75254081+saivojjala@users.noreply.github.com>
2020-12-23 22:35:19 +0530

    Add files via upload

--- Week  3/server.py
+++ Week  3/server.py
@@ -1,1 +1,11 @@
+import socket
+
+serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+serversocket.bind((socket.gethostname(), 2002))
+serversocket.listen(5)
+
+while True:
+    clientsocket, address = serversocket.accept()
+    clientsocket.send("121311231232132421AIANDAUTOMATION12131243214325124")
+    
 
