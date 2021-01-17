98dfe5f0c9d5866ffa9d4a5364238858beaae062
saivojjala <75254081+saivojjala@users.noreply.github.com>
2021-01-17 15:47:43 +0530

    Add files via upload

--- Week 4/publisher.py
+++ Week 4/publisher.py
@@ -1,1 +1,28 @@
+#!/usr/bin/env python
 
+import rospy
+from geometry_msgs.msg import Twist
+
+def publisher(linear_velocity, angular_velocity):
+	pub = rospy.Publisher("/turtle1/cmd_vel", Twist, queue_size=10)
+	rospy.init_node("publisher", anonymous=True)
+	rate = rospy.Rate(10)
+	velocity=Twist()
+
+	while not rospy.is_shutdown():
+
+		velocity.linear.x = linear_velocity
+		velocity.linear.y = 0
+		velocity.linear.z = 0
+
+		velocity.angular.x = 0
+		velocity.angular.y = 0
+		velocity.angular.z = angular_velocity
+
+		rospy.loginfo("Linear velocity = %f Angular velocity = %f", linear_velocity, angular_velocity)
+		pub.publish(velocity)
+		rate.sleep()
+
+if __name__ == "__main__":
+	publisher(3.0, 3.0)
+	
