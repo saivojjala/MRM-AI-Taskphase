b47bfd57600e257af2482af7ec327eccad7b7b21
saivojjala <75254081+saivojjala@users.noreply.github.com>
2021-06-25 20:49:39 +0530

    Add files via upload

---
+++ PCL Task/clusters.cpp
@@ -0,0 +1,143 @@
+#include <ros/ros.h>
+#include <iostream>
+#include <pcl_ros/point_cloud.h>
+#include <pcl_conversions/pcl_conversions.h>
+#include <sensor_msgs/PointCloud2.h>
+#include <pcl/ModelCoefficients.h>
+#include <pcl/io/pcd_io.h>
+#include <pcl/point_types.h>
+#include <pcl/sample_consensus/method_types.h>
+#include <pcl/sample_consensus/model_types.h>
+#include <pcl/segmentation/sac_segmentation.h>
+#include <pcl/filters/voxel_grid.h>
+#include <pcl/filters/extract_indices.h>
+#include <pcl/features/normal_3d.h>
+#include <pcl/kdtree/kdtree.h>
+#include <pcl/segmentation/extract_clusters.h>
+
+ros::Publisher pub;
+
+
+void clusters_cb(const sensor_msgs::PointCloud2ConstPtr& input)
+{
+    
+    pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud_blob (new pcl::PointCloud<pcl::PointXYZRGB>);
+    pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud_filtered (new pcl::PointCloud<pcl::PointXYZRGB>);
+    pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud_p (new pcl::PointCloud<pcl::PointXYZRGB>);
+    pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud_f (new pcl::PointCloud<pcl::PointXYZRGB>);
+
+    // Convert to the templated PointCloud
+    pcl::fromROSMsg (*input, *cloud_blob);
+
+    // Create the filtering object: downsample the dataset using a leaf size of 1cm
+    pcl::VoxelGrid<pcl::PointXYZRGB> sor;
+    sor.setInputCloud (cloud_blob);
+    sor.setLeafSize (0.015f, 0.015f, 0.015f);
+    sor.filter (*cloud_filtered);
+    
+    pcl::ModelCoefficients::Ptr coefficients (new pcl::ModelCoefficients ());
+    pcl::PointIndices::Ptr inliers (new pcl::PointIndices ());
+    // Create the segmentation object
+    pcl::SACSegmentation<pcl::PointXYZRGB> seg;
+    // Optional
+    seg.setOptimizeCoefficients (true);
+    // Mandatory
+    seg.setModelType (pcl::SACMODEL_PARALLEL_PLANE);
+    seg.setMethodType (pcl::SAC_RANSAC);
+    seg.setMaxIterations (1000);
+    seg.setDistanceThreshold (0.01);
+
+    // Create the filtering object
+    pcl::ExtractIndices<pcl::PointXYZRGB> extract;
+
+    int i = 0, nr_points = (int) cloud_filtered->size ();
+    // While 30% of the original cloud is still there
+    while (cloud_filtered->size () > 0.7 * nr_points)
+    {
+        // Segment the largest planar component from the remaining cloud
+        seg.setInputCloud (cloud_filtered);
+        seg.segment (*inliers, *coefficients);
+        if (inliers->indices.size () == 0)
+        {
+            std::cerr << "Could not estimate a planar model for the given dataset." << std::endl;
+            break;
+        }
+
+        // Extract the inliers
+        extract.setInputCloud (cloud_filtered);
+        extract.setIndices (inliers);
+        extract.setNegative (false);
+        extract.filter (*cloud_p);
+
+        // Create the filtering object
+        extract.setNegative (true);
+        extract.filter (*cloud_f);
+        cloud_filtered.swap (cloud_f);
+        i++;
+    }
+
+    // Creating the KdTree object for the search method of the extraction
+    pcl::search::KdTree<pcl::PointXYZRGB>::Ptr tree (new pcl::search::KdTree<pcl::PointXYZRGB>);
+    tree->setInputCloud (cloud_filtered);
+
+    std::vector<pcl::PointIndices> cluster_indices;
+    pcl::EuclideanClusterExtraction<pcl::PointXYZRGB> ec;
+    ec.setClusterTolerance (0.02); // 2cm
+    ec.setMinClusterSize (10);
+    ec.setMaxClusterSize (25000);
+    ec.setSearchMethod (tree);
+    ec.setInputCloud (cloud_filtered);
+    ec.extract (cluster_indices);
+
+
+    pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud_cluster (new pcl::PointCloud<pcl::PointXYZRGB>);
+    int j = 0;
+
+    for (std::vector<pcl::PointIndices>::const_iterator it = cluster_indices.begin (); it != cluster_indices.end (); ++it)
+    {
+
+        int red = rand();
+        int blue = rand();
+        int green = rand();
+        for (std::vector<int>::const_iterator pit = it->indices.begin (); pit != it->indices.end (); ++pit)
+        {
+            
+        
+            cloud_filtered->points[*pit].r = red;
+            cloud_filtered->points[*pit].b = blue;
+            cloud_filtered->points[*pit].g = green;
+            
+            cloud_cluster->points.push_back (cloud_filtered->points[*pit]); //*
+        }
+
+        j++;
+    }
+
+    cloud_cluster->width = cloud_cluster->points.size ();
+    cloud_cluster->height = 1;
+    cloud_cluster->is_dense = true;
+
+
+    sensor_msgs::PointCloud2 final_output;
+    pcl::PCLPointCloud2 output;
+
+    pcl::toPCLPointCloud2 (*cloud_cluster, output);
+    pcl_conversions::fromPCL (output, final_output);
+
+    final_output.header.frame_id = "kinect_link";
+    
+    pub.publish (final_output);
+}
+
+int main(int argc, char** argv)
+{
+    ros::init(argc, argv, "clusters");
+    ros::NodeHandle n;
+
+    ros::Subscriber sub = n.subscribe<sensor_msgs::PointCloud2>("kinect/depth/points", 1, clusters_cb);
+  
+    pub = n.advertise<sensor_msgs::PointCloud2> ("/pcl_new", 1);
+
+    ros::spin();
+}
+
