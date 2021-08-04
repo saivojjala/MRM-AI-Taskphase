656dcd0815404a4eb0f795694a7d9cd12a42fad5
saivojjala <75254081+saivojjala@users.noreply.github.com>
2021-08-04 11:51:29 +0530

    Delete Week 3 directory

--- Week 3/client.py
+++
@@ -1,8 +0,0 @@
-import socket 
-
-clientsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
-clientsocket.connect((socket.gethostname(), 2002)) 
-
-from_server = clientsocket.recv(1024)
-print(from_server)
-
